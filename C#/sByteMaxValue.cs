92b3612301de883e1230f260ecabf5cd0f6466be
souvikiiest <32746412+souvikiiest@users.noreply.github.com>
2021-11-10 12:44:27 +0530

    Create sByteMaxValue.cs
    Leo has just started to learn about data types.  His first assignment is to find out the largest value that can be stored in a signed byte. 
    
    Write a program to declare a variable named 'number' of type signed byte, initialize it to 125 and display it.
    
    Change the value of number to the maximum value of a signed byte and display it as shown in the sample output.
    
     
    
    Sample output
    
    Value of number: 125
    
    Largest value stored in a signed byte :   //print the answer.

---
+++ C#/sByteMaxValue.cs
@@ -0,0 +1,10 @@
+using System;
+public class Program
+{
+public static void Main(syting[] args)
+{
+int number=125;
+Console.WriteLine("Value of number:{0}",number);
+Console.WriteLine("Largest value stored in a signed byte:{0}",sbyte.MaxValue);
+}
+}
